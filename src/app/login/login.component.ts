fa4ff406ab8e3594b6e26d5d243d34cd6b0e85cd
tuankhanh45 <tuankhanhtran45@gmail.com>
2019-07-10 15:43:29 +0700

    add form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class LoginComponent {
+  log(x) { console.log(x); }
 }
