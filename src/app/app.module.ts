cf81856f1018995f549346f5f76403e83b7f6bbf
tuankhanh45 <tuankhanhtran45@gmail.com>
2019-07-11 09:15:27 +0700

    creat register form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms'
+import {FormsModule,ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from '../app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -21,7 +21,8 @@ import { MenuNavComponent } from './menu_nav/menu_nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
